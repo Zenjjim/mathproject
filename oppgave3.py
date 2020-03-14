d206fedf12239d17a0bea3ec2a345694635d8737
zuimran <zuimran@stud.ntnu.no>
2020-03-14 16:59:57 +0100

    finish opg5

--- oppgave3.py
+++ oppgave3.py
@@ -2,18 +2,18 @@ from scipy.sparse.linalg import spsolve
 from numpy import power
 from oppgave2 import lagA
 
-def calc_y_c():
-  w = 0.3 #m
-  t = 0.03 #m
-  g = 9.81 #N
-  d = 480 #kg/m^3
-  L = 2 #m
-  E = 1.3 * 10**10 #N/m^2
-  n = 10
-  I = (w*t**3)/12
-  f = -d*w*t*g
-  h = L/n
-  f_x = [f] * n
-  A = lagA(n)
-  y = spsolve(A, f_x)
-  return y
+
+def calc_y_c(n):
+    w = 0.3  # m
+    t = 0.03  # m
+    g = 9.81  # N
+    d = 480  # kg/m^3
+    L = 2  # m
+    E = 1.3 * 10**10  # N/m^2
+    I = (w*t**3)/12
+    f = -d*w*t*g
+    h = L/n
+    f_x = [(h ** 4 / (E * I)) * f] * n
+    A = lagA(n)
+    y = spsolve(A, f_x)
+    return y
--- oppgave5.py
+++ oppgave5.py
@@ -1,29 +1,28 @@
 from scipy.sparse.linalg import spsolve, inv, norm
+from scipy.sparse import csc_matrix
+from numpy.linalg import cond
 from math import inf
 from oppgave2 import lagA
+from oppgave3 import calc_y_c
+
 
 def oppgave5():
-  w = 0.3 #m
-  t = 0.03 #m
-  g = 9.81 #N
-  d = 480 #kg/m^3
-  L = 2 #m
-  x = L
-  E = 1.3 * 10**10 #N/m^2
-  I = (w*t**3)/12
-  f_x = -d*w*t*g
-  for i in range (1, 12):
-    n = 10 * 2 ** i
-    h = L/n
+    w = 0.3  # m
+    t = 0.03  # m
+    g = 9.81  # N
+    d = 480  # kg/m^3
+    L = 2  # m
+    x = L
+    E = 1.3 * 10**10  # N/m^2
+    I = (w*t**3)/12
+    f_x = -d*w*t*g
+    y = f_x / (24 * E * I) * (x ** 2) * (x ** 2 - 4 * L * x + 6 * L ** 2)
 
-    m = [(h ** 4) / (E * I) * f_x] * n
-
-    y = f_x / (24 * E * I) * x ** 2 * (x ** 2 - 4 * L * x + 6 * L ** 2)
-    
-    A = lagA(n)
-    invA = inv(A)
-
-    print("Eksponent: ", i, ", n = ", n)
-    print("Kondisjonstall: ", norm(A, ord=inf) * norm(invA, ord=inf))
+    for i in range(1, 10):
+        n = 10 * 2 ** i
+        m = calc_y_c(n)
+        print("Eksponent:", i, " n=", n,
+              "Kondisjonstall:", cond(lagA(n), p=inf),
+              "Feil (x=L):", y-m[-1])
 
 oppgave5()
